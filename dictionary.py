1620b1e46e66c3abff87f16c6fea8c29db7bb4d1
User <@laurazhangalieva93-art>
2026-01-27 12:57:59 +0600

    create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,6 @@
+email = {
+    "subject": "Everyweekly meeting",
+    "from": "laurazhangaliyeva@gmail.com. ",
+    "to": "kimnastya@gmail.com.",
+    "body": "Hello!\n\tI want to announce the meeting on Wednesday.\n\tPlease review and let me know your feedback."
+}
