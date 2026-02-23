21afc3e6f6a06b9b3456b3da108ef6256763ce19
User <@laurazhangalieva93-art>
2026-02-23 12:01:37 +0600

    reformate code

--- email.py
+++ email.py
@@ -7,14 +7,15 @@ email = {
     "body": "Hello!\n\tI want to announce the meeting on Wednesday.\n\tPlease review and let me know your feedback."
 }
 from datetime import date
+
 send_date = str(date.today())
 email["date"] = send_date
 print(email)
 
-email ["from"] = email["from"].strip().lower()
+email["from"] = email["from"].strip().lower()
 print(email)
 
-email ["to"] = email["to"].strip().lower()
+email["to"] = email["to"].strip().lower()
 print(email)
 
 login, domain = email["from"].split("@")
@@ -24,49 +25,48 @@ print(domain)
 email["short_body"] = email["body"][:10] + "..."
 print(email["short_body"])
 
-personal_domens = [
-"gmail.com",
-"list.ru",
-"yahoo.com",
-"outlook.com",
-"hotmail.ru",
-"icloud.com",
-"yandex.ru"
-"bk.ru"
+personal_domeins = [
+    "gmail.com",
+    "list.ru",
+    "yahoo.com",
+    "outlook.com",
+    "hotmail.ru",
+    "icloud.com",
+    "yandex.ru"
+    "bk.ru"
 ]
 
-corporate_domens= [
-"company.ru",
-"corporation.com",
-"university.edu",
-"organisation.org"]
+corporate_domeins = [
+    "company.ru",
+    "corporation.com",
+    "university.edu",
+    "organisation.org"]
 
-personal_domens = list(set(personal_domens))
-corporate_domens = list(set(corporate_domens))
+personal_domeins = list(set(personal_domeins))
+corporate_domeins = list(set(corporate_domeins))
 
-print (personal_domens)
-print(corporate_domens)
+print(personal_domeins)
+print(corporate_domeins)
 
-common_domens=list(set(personal_domens) & set(corporate_domens))
-print(common_domens)
+common_domeins = list(set(personal_domeins) & set(corporate_domeins))
+print(common_domeins)
 
-is_corporate= domain in corporate_domens
+is_corporate = domain in corporate_domeins
 print(is_corporate)
 
-clean_body= email["body"].replace("\n", " ").replace("\t", " ")
-email ["clean_body"]= clean_body
-print (email["clean_body"])
+clean_body = email["body"].replace("\n", " ").replace("\t", " ")
+email["clean_body"] = clean_body
+print(email["clean_body"])
 
-
-email ["sent_text"] =f""" Кому: {email['to']} 
+email["sent_text"] = f""" Кому: {email['to']} 
     От {email['from']}, 
     Тема: {email['subject']} 
     Дата: {email['date']} 
  {email['clean_body']}"""
-print (email["sent_text"])
+print(email["sent_text"])
 
 pages = math.ceil(len(email["clean_body"]) / 500)
-print (pages)
+print(pages)
 
 is_subject_empty = not email["subject"].strip()
 is_body_empty = not email["body"].strip()
@@ -77,12 +77,6 @@ email["masked_from"] = login[:2] + "***@" + domain
 
 print(email["masked_from"])
 
-personal_domens.remove("list.ru")
-personal_domens.remove("bk.ru")
-print(personal_domens)
-
-
-
-
-
-
+personal_domeins.remove("list.ru")
+personal_domeins.remove("bk.ru")
+print(personal_domeins)
